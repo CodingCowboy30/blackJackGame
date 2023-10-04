42ca1a86b9ea1edcf466a1a353deff8e0f26f727
Martez Carr <113547475+CodingCowboy30@users.noreply.github.com>
2023-10-03 19:28:50 -0500

    added comment

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // 1. Create two variables firstCard and secondCard.
 // Set values to a random number between 1-21
 
- let firstCard = 8
+let firstCard = 8
 let secondCard = 10
-let sum = firstCard + secondCard  // 24
+let sum = firstCard + secondCard   // 24
 let hasBlackJack = false
 let isAlive = true
+let message = ""
 
-if (sum < 21){
-    console.log("Do you want to draw a new card? 🤞")
-    
+if (sum <= 21){
+    message = "Do you want to draw a new card? 🤞"
 } else if (sum === 21) {
-    console.log("Wohoo! You've got BlackJack! 😎") 
+    message ="Wohoo! You've got BlackJack! 😎" 
     hasBlackJack = true
-} else{
-    console.log("You're out of the game!🤦‍♂️")
+} else {
+    message= "You're out of the game!🤦‍♂️"
     isAlive = false
 }
-console.log(isAlive)
+console.log(message)
 
 // CASH OUT!
 
